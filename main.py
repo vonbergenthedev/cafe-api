ca5c8489835920eb458a06b8bfa4554c914b2f57
Ashton VonBergen <vonbergenthedev@gmail.com>
2025-03-07 14:45:48 -0700

    Add Delete route

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import random
 
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.exc import StatementError, NoResultFound
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, load_only
-from sqlalchemy import Integer, String, Boolean, func
+from sqlalchemy.exc import StatementError
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean
 
 app = Flask(__name__)
 
@@ -126,6 +126,23 @@ def update_cafe_price(cafe_id):
 
 
 # HTTP DELETE - Delete Record
+@app.route('/report-closed/<cafe_id>', methods=['DELETE'])
+def cafe_closed(cafe_id):
+    api_key = request.args.get('api_key')
+
+    if api_key == 'TopSecretAPIKey':
+        cafe_to_delete = db.session.get(Cafe, cafe_id)
+
+        if cafe_to_delete:
+            db.session.delete(cafe_to_delete)
+            db.session.commit()
+            return jsonify(response={'success': 'Cafe removed from the database.'})
+
+        else:
+            return jsonify(error='Sorry, a cafe with that ID was not found in the database.'), 404
+
+    else:
+        return jsonify(error='Sorry, that\'s not allowed. Make sure you have the correct api_key.'), 403
 
 
 if __name__ == '__main__':
