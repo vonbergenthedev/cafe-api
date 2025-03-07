090ed0d9bbb705c46e5f7f2cd89c2f559d993323
Ashton VonBergen <vonbergenthedev@gmail.com>
2025-03-07 12:24:56 -0700

    Create get all cafes route

--- main.py
+++ main.py
@@ -46,6 +46,16 @@ class Cafe(db.Model):
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
+    def to_dict(self):
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
 
 with app.app_context():
     db.create_all()
@@ -60,26 +70,24 @@ def home():
 @app.route("/random", methods=["GET"])
 def get_random_cafe():
     with app.app_context():
-        data = db.session.execute(db.select(Cafe))
-        all_cafes = data.scalars().all()
+        result = db.session.execute(db.select(Cafe))
+        all_cafes = result.scalars().all()
         random_cafe = random.choice(all_cafes)
 
-    return jsonify(cafe={
-        'id': random_cafe.id,
-        'name': random_cafe.name,
-        'map_url': random_cafe.map_url,
-        'img_url': random_cafe.img_url,
-        'location': random_cafe.location,
-        'seats': random_cafe.seats,
-        'has_toilet': random_cafe.has_toilet,
-        'has_wifi': random_cafe.has_wifi,
-        'has_sockets': random_cafe.has_sockets,
-        'can_take_calls': random_cafe.can_take_calls,
-        'coffee_price': random_cafe.coffee_price,
-    })
+        return jsonify(cafe=random_cafe.to_dict())
+
+
+@app.route("/all", methods=["GET"])
+def get_all_cafes():
+    with app.app_context():
+        result = db.session.execute(db.select(Cafe))
+        all_cafes = result.scalars().all()
+
+        return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
 
 
 # HTTP POST - Create Record
+
 
 # HTTP PUT/PATCH - Update Record
 
